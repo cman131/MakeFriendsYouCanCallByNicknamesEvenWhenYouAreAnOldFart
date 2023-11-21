98922dc48d7f5974a68d751e7e012e0bd925e2d3
Cman131 <conor13@msn.com>
2023-11-21 08:03:45 -0500

    handle private users

--- app.js
+++ app.js
@@ -62,7 +62,11 @@ schedule.scheduleJob({
                     for (let member of values) {
                         let user = member.user;
                         console.log(`Messaged ${user.username} about the ${event.name} event.`);
-                        user.send(`**${event.name}** is coming up tomorrow and you are signed up to join! If your plans have changed, please update your status on the event.\n${event.url}`);
+                        try {
+                          user.send(`**${event.name}** is coming up tomorrow and you are signed up to join! If your plans have changed, please update your status on the event.\n${event.url}`);
+                        } catch (e) {
+                          console.log(`Failed to message ${user.username} about ${event.name}. Error: ${e}`)
+                        }
                     }
                 });
             }
